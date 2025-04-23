a0f13ee8cea6ed6fae782a6e9f923464db5009ab
Siddharth Ray <siddharth.ray23@gmail.com>
2025-04-23 07:55:25 -0400

    counter app v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
+export default function Counter() {
+  const initialCount = 0;
+  const [count, setCount] = React.useState(initialCount);
+
+  const handleClick = (operation) => {
+    if (operation === "+") {
+      setCount((prev) => prev + 1);
+    } else if (operation === "-") {
+      if (count === 0) {
+        return;
+      }
+      setCount((prev) => prev - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter</h1>
+      <h2>{count}</h2>
+      <p>You clicked {count} times</p>
+      <button onClick={() => handleClick("+")}>Add</button>
+      <button disabled={count <= 0} onClick={() => handleClick("-")}>
+        Subtract
+      </button>
+      <button onClick={() => setCount(initialCount)}>Reset</button>
     </div>
   );
 }
-
-export default App;
