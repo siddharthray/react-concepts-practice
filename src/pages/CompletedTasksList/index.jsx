e9adb8662797a9918317d26f8381c8ef7dfab744
Siddharth Ray <siddharth.ray23@gmail.com>
2025-05-09 17:03:19 -0400

    completed task updated with restructured route

--- src/pages/CompletedTasksList/index.jsx
+++ src/pages/CompletedTasksList/index.jsx
@@ -1,22 +1,23 @@
 // pages/OpenTasksPage.tsx
-import React from "react";
 import CompletedTasksList from "./CompletedTasksPage.module.css";
 import TodoList from "../../features/todo/todoList";
+import { useTasks } from "../../hooks/useTasks";
 
-export default function CompletedTasksPage({
-  doneTasks,
-  onDelete,
-  onToggle,
-  title,
-}) {
+export default function CompletedTasksPage({ title }) {
   // const doneTasks = tasks.filter((t) => t?.completed);
+  const { doneTasks, removeTask, toggleTask, saveEdit } = useTasks();
 
   return (
     <div
       className={`${CompletedTasksList.completedTaskscolumn} ${CompletedTasksList.completedTasks}`}
     >
       <h2 className={CompletedTasksList.title}>{title}</h2>
-      <TodoList items={doneTasks} onDelete={onDelete} onToggle={onToggle} />
+      <TodoList
+        items={doneTasks}
+        onDelete={removeTask}
+        onToggle={toggleTask}
+        onEdit={saveEdit}
+      />
     </div>
   );
 }
--- src/pages/OpenTasksList/index.jsx
+++ src/pages/OpenTasksList/index.jsx
@@ -1,17 +1,11 @@
 // pages/OpenTasksPage.tsx
-import React from "react";
 import TodoList from "../../features/todo/todoList";
 import openTasksList from "./OpenTasksPage.module.css";
 import { useTasks } from "../../hooks/useTasks";
 
 export default function OpenTasksPage({ title = "Open Tasks" }) {
   // const openTasks = tasks.filter((t) => !t?.completed);
-  const {
-    openTasks,
-    removeTask,
-    toggleTask,
-    saveEdit, // if you allow editing here
-  } = useTasks();
+  const { openTasks, removeTask, toggleTask, saveEdit } = useTasks();
 
   return (
     <div
